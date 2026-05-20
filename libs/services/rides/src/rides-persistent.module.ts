7c90e4e20cd2df79a763366105d3aa70bc327807
Kaushal Regmi <regmikaushal321@gmail.com>
2026-05-20 23:03:00 +0545

    Fixed vechile refrence error

--- libs/services/rides/src/rides-persistent.module.ts
+++ libs/services/rides/src/rides-persistent.module.ts
@@ -1,4 +1,4 @@
-import { Rides, RidesSchema, RidesRepository } from "@libs/data-access";
+import { Rides, RidesSchema, RidesRepository, User, Vehicle, VehicleSchema, UserSchema } from "@libs/data-access";
 import { Module } from "@nestjs/common";
 import { MongooseModule } from "@nestjs/mongoose";
 
@@ -6,6 +6,8 @@ import { MongooseModule } from "@nestjs/mongoose";
     imports: [
         MongooseModule.forFeature([
             { name: Rides.name, schema: RidesSchema },
+            { name: User.name, schema: UserSchema },
+            { name: Vehicle.name, schema: VehicleSchema },
         ]),
     ],
     providers: [
