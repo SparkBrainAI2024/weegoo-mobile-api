45563dd249d7402b7f5b3aa7f5e476a79bdc2694
Kaushal Regmi <regmikaushal321@gmail.com>
2026-05-26 22:17:53 +0545

    Role Guard implement for user

--- apps/api/src/modules/user-favourites/user-favourites.resolver.ts
+++ apps/api/src/modules/user-favourites/user-favourites.resolver.ts
@@ -1,45 +1,48 @@
 import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
 import { UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@libs/guards';
-import { CurrentUser } from '@libs/common';
-import { 
-  User, 
-  Favourites, 
-  PaginationInput, 
-  CreateFavouriteInput ,
-  FavouriteListWithPaginationResponse,
-  PaginationInputOnly
+import { CurrentUser, Roles, ROLES_KEY } from '@libs/common';
+import {
+    User,
+    Favourites,
+    CreateFavouriteInput,
+    FavouriteListWithPaginationResponse,
+    PaginationInputOnly,
+    roles
 } from '@libs/data-access';
 import { FavouriteService } from '@libs/services/favourites/favourites.service';
+import { RoleGuard } from '@libs/guards/role.guard';
 
 @Resolver(() => Favourites)
-@UseGuards(AuthGuard)
+@UseGuards(AuthGuard, RoleGuard)
 export class FavouritesResolver {
-  constructor(private readonly favouriteService: FavouriteService) {}
+    constructor(private readonly favouriteService: FavouriteService) { }
+    @Roles(roles.USER)
+    @Query(() => FavouriteListWithPaginationResponse, { name: 'getMyFavourites' })
+    async getMyFavourites(
+        @CurrentUser() user: User,
+        @Args('options', { nullable: true }) options: PaginationInputOnly,
+    ) {
+        return this.favouriteService.findRides(user, options);
+    }
 
-  @Query(() => FavouriteListWithPaginationResponse, { name: 'getMyFavourites' })
-  async getMyFavourites(
-    @CurrentUser() user: User,
-    @Args('options', { nullable: true }) options: PaginationInputOnly,
-  ) {
-    return this.favouriteService.findRides(user, options);
-  }
-
-  @Mutation(() => Favourites, { name: 'createFavourite' })
-  async createFavourite(
-    @CurrentUser() user: User,
-    @Args('input') input: CreateFavouriteInput,
-  ) {
-    return this.favouriteService.createFavorite({
-      ...input,
-      passengerId: user._id,
-    } as any);
-  }
-  @Query(() => Favourites, { name: 'getFavouriteById' })
-  async getFavouriteById(
-    @CurrentUser() user: User,
-    @Args('favouriteId') favouriteId: string,
-  ) {
-    return this.favouriteService.getFavouriteById(favouriteId, user._id.toString());
-  }
+    @Roles(roles.USER)
+    @Mutation(() => Favourites, { name: 'createFavourite' })
+    async createFavourite(
+        @CurrentUser() user: User,
+        @Args('input') input: CreateFavouriteInput,
+    ) {
+        return this.favouriteService.createFavorite({
+            ...input,
+            passengerId: user._id,
+        } as any);
+    }
+    @Roles(roles.USER)
+    @Query(() => Favourites, { name: 'getFavouriteById' })
+    async getFavouriteById(
+        @CurrentUser() user: User,
+        @Args('favouriteId') favouriteId: string,
+    ) {
+        return this.favouriteService.getFavouriteById(favouriteId, user._id.toString());
+    }
 }
