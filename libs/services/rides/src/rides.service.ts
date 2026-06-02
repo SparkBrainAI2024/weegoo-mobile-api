16c7268e441f0ebaf886f6c32737e6515deb92e4
Kaushal Regmi <regmikaushal321@gmail.com>
2026-06-02 22:20:59 +0545

    Fixed ride service

--- libs/services/rides/src/rides.service.ts
+++ libs/services/rides/src/rides.service.ts
@@ -1,4 +1,4 @@
-import { PaginationInput, RidesRepository, User, RidesDocument, RideStatus, RideTypes, ProvinceEnum, roles, UserDetailsRepository, PromoCode, PromoCodeDocument, PromoCodeStatusEnum, DiscountTypeEnum } from '@libs/data-access';
+import { PaginationInput, RidesRepository, User, RidesDocument, RideStatus, RideTypes, ProvinceEnum, roles, UserDetailsRepository, DiscountTypeEnum, PromoCodeStatusEnum, PromoCode, PromoCodeUsed, PromoCodeDocument, PromoCodeUsedDocument } from '@libs/data-access';
 import { Model, Types } from 'mongoose';
 import {  HttpStatus, Injectable } from '@nestjs/common';
 import { TransactionService } from '@libs/services/payment/src/transaction/transaction.service';
