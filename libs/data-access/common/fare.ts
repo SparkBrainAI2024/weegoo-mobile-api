dad2c14a1173472c0bb2601150a7a500097f32fb
rabinabaga-sparkai <rabina@sparkbrainai.com>
2026-07-05 15:33:32 +0545

    converted to number instead of Number in fare entity

--- libs/data-access/common/fare.ts
+++ libs/data-access/common/fare.ts
@@ -4,40 +4,38 @@ import { ApiProperty } from "@nestjs/swagger";
 import { Types } from "mongoose";
 @ObjectType()
 export class Fare {
-    @Field(() => Number,{defaultValue: 0})
-    @Prop({ type: Number, default: 0, required: false,nullable:true })
-    @ApiProperty({ nullable: false })
-    baseAmount: Number;
+  @Field(() => Number, { defaultValue: 0 })
+  @Prop({ type: Number, default: 0, required: false, nullable: true })
+  @ApiProperty({ nullable: false })
+  baseAmount: number;
 
-   @Field(() => Number,{defaultValue: 0})
-    @Prop({ type: Number, default: 0, required: false,nullable:true })
-    @ApiProperty({ nullable: false })
-    trafficCongestionAmount: Number;
+  @Field(() => Number, { defaultValue: 0 })
+  @Prop({ type: Number, default: 0, required: false, nullable: true })
+  @ApiProperty({ nullable: false })
+  trafficCongestionAmount: number;
 
-    @Field(() => Number,{defaultValue: 0})
-    @Prop({ type: Number, default: 0, required: false,nullable:true })
-    @ApiProperty({ nullable: false })
-    distanceAmount: Number;
+  @Field(() => Number, { defaultValue: 0 })
+  @Prop({ type: Number, default: 0, required: false, nullable: true })
+  @ApiProperty({ nullable: false })
+  distanceAmount: number;
 
-    @Field(() => Number,{defaultValue: 0})
-    @Prop({ type: Number, default: 0, required: false,nullable:true })
-    @ApiProperty({ nullable: false })
-    totalAmount: Number;
+  @Field(() => Number, { defaultValue: 0 })
+  @Prop({ type: Number, default: 0, required: false, nullable: true })
+  @ApiProperty({ nullable: false })
+  totalAmount: number;
 
-    @Field(() => Number,{defaultValue: 1})
-    @Prop({ type: Number, default: 1, required: false,nullable:true })
-    @ApiProperty({ nullable: false })
-    noOfPassengers: Number;
+  @Field(() => Number, { defaultValue: 1 })
+  @Prop({ type: Number, default: 1, required: false, nullable: true })
+  @ApiProperty({ nullable: false })
+  noOfPassengers: number;
 
+  @Field(() => Number, { defaultValue: 0 })
+  @Prop({ type: Number, default: 0, required: false, nullable: true })
+  @ApiProperty({ nullable: false })
+  discountAmount: number;
 
-    @Field(() => Number,{defaultValue: 0})
-    @Prop({ type: Number, default: 0, required: false,nullable:true })
-    @ApiProperty({ nullable: false })
-    discountAmount: Number;
-
-    @Field(() => String, { nullable: true })
-    @Prop({ type: Types.ObjectId, ref: 'PromoCode', default: null })
-    @ApiProperty({ nullable: true })
-    promoCodeId?: Types.ObjectId;
-
+  @Field(() => String, { nullable: true })
+  @Prop({ type: Types.ObjectId, ref: "PromoCode", default: null })
+  @ApiProperty({ nullable: true })
+  promoCodeId?: Types.ObjectId;
 }
