6799303ea573e3f4756118172ed5dc2d134b300e
rabinabaga-sparkai <rabina@sparkbrainai.com>
2026-07-12 12:06:24 +0545

    fix dependency injection issues

--- libs/services/notification/src/notification.module.ts
+++ libs/services/notification/src/notification.module.ts
@@ -5,16 +5,22 @@ import { NotificationResolver } from "./resolver/notification.resolver";
 // Import FirebaseMessagingService - adjust the path if it is located elsewhere
 import { FirebaseMessagingService } from "@libs/services/firebase-messaging";
 import { ConfigModule } from "@nestjs/config";
+import { EnvService } from "@libs/common/config/env.service";
+import { userModel } from "@libs/data-access";
+import { MongooseModule } from "@nestjs/mongoose";
 
 @Module({
-    imports: [
-        NotificationPersistentModule,
-        ConfigModule,
-    ],
-    providers: [ 
-        FirebaseMessagingService, 
-        NotificationService, 
-        NotificationResolver
-    ]
+  imports: [
+    MongooseModule.forFeature([userModel]),
+    NotificationPersistentModule,
+    ConfigModule,
+  ],
+  providers: [
+    FirebaseMessagingService,
+    NotificationService,
+    NotificationResolver,
+    EnvService,
+  ],
+  exports: [NotificationService],
 })
-export class NotificationModule { }
+export class NotificationModule {}
