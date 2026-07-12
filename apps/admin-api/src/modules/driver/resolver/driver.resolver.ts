6693b763e2f8f20b1e3cf0fc31c9a1e463068766
rabinabaga-sparkai <rabina@sparkbrainai.com>
2026-07-12 12:03:32 +0545

    build fix

--- apps/admin-api/src/modules/driver/resolver/driver.resolver.ts
+++ apps/admin-api/src/modules/driver/resolver/driver.resolver.ts
@@ -10,18 +10,14 @@ import {
 } from "@nestjs/graphql";
 import { CurrentLang } from "@libs/common/decorators/header.decorators";
 import { DriverService } from "@libs/services/driver/driver.service";
-import { SetMetadata, UseGuards } from "@nestjs/common";
-import { AuthGuard, RoleGuard } from "@libs/guards";
-import { BasicResponse, DriverDocument, roles, User } from "@libs/data-access";
+
+import { DriverDocument } from "@libs/data-access";
 import { DriverDocumentService } from "@libs/services/driver-document/driver-document.service";
 import {
   DriverListItem,
   DriverListResponse,
 } from "@libs/data-access/dtos/response/driver-list.response";
-import {
-  DriverListInput,
-  ToggleBlockDriverInput,
-} from "@libs/data-access/dtos/input/driver-list.input";
+import { DriverListInput } from "@libs/data-access/dtos/input/driver-list.input";
 import { DeleteDriverInput } from "@libs/data-access/dtos/input/delete-driver.input";
 
 // @UseGuards(AuthGuard, RoleGuard)
